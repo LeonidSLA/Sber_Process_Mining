88d080a4618fc356eacdc7e50e1b72d014069b5b
gkalin <grigoriikalin@gmail.com>
2021-09-01 16:16:04 +0300

    Graph embedding

---
+++ sberpm/ml/vectorizer/_graph_embedding/_proximity_measures.py
@@ -0,0 +1,14 @@
+import functools
+import numpy
+
+def katz_index(A: np.matrix) -> :
+"""
+    Calculates the katz centrality of a graph
+"""
+def rooted_pr():
+
+def common_neigbors():
+
+def adamic_adar():
+
+
